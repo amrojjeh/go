da13ce0ae063cdd9f85e9bc1deaf76ec539ad2ab
Erregea5 <blastbaja15@gmail.com>
2024-07-07 15:39:36 -0700

    sebi

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 const JSFILE = `
@@ -10,18 +11,22 @@ const JSFILE = `
 	<body>
 		<script>
 			const evtSource = new EventSource("sse");
-			evtSource.addEventListener("ping", (e) => {
-				console.log(e);
-			});
+			// evtSource.addEventListener("ping", (e) => {
+			// 	console.log(e);
+			// });
+			evtSource.onmessage = function(event) {
+				console.log(event)
+		};
 		</script>
 	</body>
 	</html> `
 
-type Game struct {
-	ongoing chan
-}
+// type Game struct {
+// 	ongoing chan
+// }
 
 func main() {
+	buffer:=make(map int[Game])
 	log.Print("listening on :8080")
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +35,18 @@ func main() {
 	})
 
 	router.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request) {
-		
+		w.Header().Add("X-Accel-Buffering", "no")
+		w.Header().Add("Content-Type", "text/event-stream")
+		w.Header().Add("Cache-Control", "no-cache")
+		for range 5 {
+			w.Write([]byte(`event: ping
+
+data: {hi:"ji"}
+
+`))
+			w.(http.Flusher).Flush()
+			time.Sleep(time.Second * 5)
+		}
 	})
 
 	http.ListenAndServe(":8080", router)
